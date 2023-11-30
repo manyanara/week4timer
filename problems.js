8bc70ad71b9cf166b825dc4c23a22d6dd09518ff
Manya <Manya@Khangals-MacBook-Air.local>
2023-11-29 21:39:18 -0800

    finalized renderScores(), will be adding CSS after this

--- problems.js
+++ problems.js
@@ -1,23 +1,13 @@
 
 // start timer once problem.html is opened and pull out of the game when the timer runs out
 var TimeID = document.querySelector("#time")
-var secondsLeft = 60;
-
-function setTime() {
-    var timerInterval = setInterval(function () {
-        secondsLeft--;
-        TimeID.textContent = secondsLeft;
-
-        if (secondsLeft <= 0) {
-            clearInterval(timerInterval);
-            TimeID.textContent = "Times Up!"
-        }
-    }, 1000);
-}
+var secondsLeft = 30;
+var questionIndex = 0;
 
 // create operations that add or subtract time if specific items are selected AND pulls up a new question
-var questionIndex = 0;
+
 var score = 0;
+
 
 const questions = [
     {
@@ -61,7 +51,27 @@ const questions = [
         "D": "Bones"
     },
 ];
-// inputs question text 
+// // secondsLeft <= 0 || 
+// console.log(questionIndex)
+// if (questionIndex >= questions.length) {
+//     // clearInterval(timerInterval);
+//     // TimeID.textContent = "Times Up!"
+//     setScore();
+// }
+
+function setTime() {
+    var timerInterval = setInterval(function () {
+        secondsLeft--;
+        TimeID.textContent = secondsLeft;
+
+        if (secondsLeft <= 0 || questionIndex >= 5) {
+            clearInterval(timerInterval);
+            TimeID.textContent = "Times Up!"
+            setScore();
+        }
+    }, 1000);
+}
+/////////////////////////////// inputs question text 
 function displayQuestions() {
     var question = questions[questionIndex];
 
@@ -71,86 +81,81 @@ function displayQuestions() {
     var option3 = document.getElementById("answer3")
     var option4 = document.getElementById("answer4")
 
+    console.log("display question" , questionIndex);
+    // if (questionIndex >= questions.length){
+    //     console.log('test')
+    //     // setScore();
+    // }
+
     questionEl.textContent = question.text;
     option1.textContent = question.A;
     option2.textContent = question.B;
     option3.textContent = question.C;
     option4.textContent = question.D;
+
+  
 }
  //adds to score based on true/false values and moves to next question
 // function checkAnswer
 
-document.getElementById("answer1").addEventListener("click", function checkAnswer() {
+document.getElementById("answer1").addEventListener("click", function checkAnswer1() {
     var question = questions[questionIndex];
     if (this.textContent === question.correct_answer){
-        score ++,
+        score++,
         setTime(secondsLeft + 5)
-        questionIndex += 1,
-        displayQuestions(questionIndex)
-    } else
-    questionIndex += 1, 
-    setTime(secondsLeft-5)
-    displayQuestions(questionIndex)
-
-    if (questionIndex == 5 || secondsLeft === 0){
-        setScore();
+    } else{
+       setTime(secondsLeft-5)
     }
-});
-document.getElementById("answer2").addEventListener("click", function checkAnswer() {
-    var question = questions[questionIndex];
-    if (this.textContent === question.correct_answer){
-        score ++,
-        setTime(secondsLeft + 5)
-        questionIndex += 1,
-        displayQuestions(questionIndex)
-    } else
-    questionIndex += 1, 
-    setTime(secondsLeft-5)
-    displayQuestions(questionIndex)
-
-    if (questionIndex == 5|| secondsLeft === 0){
-        setScore();
-    }
-});
-document.getElementById("answer3").addEventListener("click", function checkAnswer() {
-    var question = questions[questionIndex];
-    if (this.textContent === question.correct_answer){
-        score ++,
-        setTime(secondsLeft + 5)
-        questionIndex += 1,
-        displayQuestions(questionIndex)
-    } else
-    questionIndex += 1, 
-    setTime(secondsLeft-5)
-    displayQuestions(questionIndex)
     
-    if (questionIndex == 5 || secondsLeft === 0){
-        setScore();
-    }
-});
 
-document.getElementById("answer4").addEventListener("click", function checkAnswer() {
+    questionIndex++,
+    displayQuestions(questionIndex)
+});
+document.getElementById("answer2").addEventListener("click", function checkAnswer2() {
     var question = questions[questionIndex];
     if (this.textContent === question.correct_answer){
-        score ++,
+        score++,
         setTime(secondsLeft + 5)
-        questionIndex += 1,
-        displayQuestions(questionIndex)
-    } else
-    questionIndex += 1, 
-    setTime(secondsLeft-5)
-    displayQuestions(questionIndex)
-
-    if (questionIndex ==5  || secondsLeft === 0){
-        setScore();
+    } else{
+       setTime(secondsLeft-5)
     }
+    
+    questionIndex++,
+    displayQuestions(questionIndex)
 });
+
+document.getElementById("answer3").addEventListener("click", function checkAnswer3() {
+    var question = questions[questionIndex];
+    if (this.textContent === question.correct_answer){
+        score++,
+        console.log('added time')
+        setTime(secondsLeft + 5)
+    } else{
+        console.log('subtracted time')
+       setTime(secondsLeft-5)
+    }
+    
+    questionIndex++,
+    displayQuestions(questionIndex)
+});
+
+document.getElementById("answer4").addEventListener("click", function checkAnswer4() {
+    var question = questions[questionIndex];
+    if (this.textContent === question.correct_answer){
+        score++,
+        setTime(secondsLeft + 5)
+    } else{
+       setTime(secondsLeft-5)
+    }
+    
+    questionIndex++,
+    displayQuestions(questionIndex)
+});
+
 // opens form to add name and score
 
 function setScore(){
     var name= window.prompt('Enter your name to view scores')
-    // localStorage.setItem("score", score);
-    // localStorage.setItem("name", name)
 
     var user = {
         firstName: name,
@@ -158,28 +163,36 @@ function setScore(){
     }
 
     localStorage.setItem("user", JSON.stringify(user));
-    
+    console.log('saved in local storage')
     renderScores();
 }
 
 function renderScores() {
     var lastGrade = JSON.parse(localStorage.getItem('user'));
-    if (lastGrade !== null){
-        document.getElementById('pastScores').style.display = 'block'
+    var history = document.getElementById(scoreList)
 
-        document.getElementById('saved-name').innerHTML = lastGrade.firstName;
-        document.getElementById('saved-grade').innerHTML = lastGrade.score;
+    if (lastGrade !== null){
+        console.log('pulled from local storage')
+        var pastScores = document.getElementById('pastScores')
+
+        pastScores.style.display = 'block'
+        
+        var li = document.createElement("li");
+        li.textContent= lastGrade.firstName + " : " + lastGrade.score,
+        pastScores.appendChild(li);
+
+        // document.getElementById('saved-name').innerHTML = lastGrade.firstName;
+        // document.getElementById('saved-grade').innerHTML = lastGrade.score;
     }
 
-    var buttons = document.getElementsByClassName('.answer')
+    var buttons = document.getElementById('buttons')
     buttons.style.display = 'none';
     
     document.getElementById('questionText').textContent= lastGrade.firstName + " received " + score + " out of 5 "
 
-    var pastScores = document.getElementById('pastScores')
-    pastScores.style.display= 'block';
 }
 
 
-displayQuestions();
+
 setTime();
+displayQuestions();
