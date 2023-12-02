d5bff9483ec76ae2ec8261d12d99a3d9ae7c5ecb
Manya <Manya@Khangals-MacBook-Air.local>
2023-12-01 19:39:20 -0800

    added readme and tried to fix some bugs

--- problems.js
+++ problems.js
@@ -4,61 +4,6 @@ var TimeID = document.querySelector("#time")
 var secondsLeft = 30;
 var questionIndex = 0;
 
-// create operations that add or subtract time if specific items are selected AND pulls up a new question
-
-var score = 0;
-
-
-const questions = [
-    {
-        "text": "What is the tallest building in the world?",
-        "correct_answer": "The Burj Khalifa", 
-        "A": "Empire States Building",
-        "B": "Salesforce Tower",
-        "C": "The Burj Khalifa",
-        "D": "The Pentagon"
-    },
-    {
-        "text": "Who is the most annoying",
-        "correct_answer": "Carrie",
-        "A": "Carrie",
-        "B": "Samantha",
-        "C": "Charlotte",
-        "D": "Miranda"
-    },
-    {
-        "text": "what city should I move to",
-        "correct_answer": "New York",
-        "A": "Oakland",
-        "B": "SLC",
-        "C": "New York",
-        "D": "Chicago"
-    },
-    {
-        "text": "What pole studio do I go to",
-        "correct_answer": "Vrv3",
-        "A": "Atomic Allure",
-        "B": "VrV3",
-        "C": "none",
-        "D": "SF Spin + Pole"
-    },
-    {
-        "text": "What is my dogs name",
-        "correct_answer": "Bones",
-        "A": "Buddy",
-        "B": "Carrie",
-        "C": "The Burj Khalifa",
-        "D": "Bones"
-    },
-];
-// // secondsLeft <= 0 || 
-// console.log(questionIndex)
-// if (questionIndex >= questions.length) {
-//     // clearInterval(timerInterval);
-//     // TimeID.textContent = "Times Up!"
-//     setScore();
-// }
-
 function setTime() {
     var timerInterval = setInterval(function () {
         secondsLeft--;
@@ -71,6 +16,54 @@ function setTime() {
         }
     }, 1000);
 }
+setTime();
+// create operations that add or subtract time if specific items are selected AND pulls up a new question
+
+var score = 0;
+
+const questions = [
+    {
+        "text": "What does HTML stand for?",
+        "correct_answer": "The Burj Khalifa", 
+        "A": "Empire States Building",
+        "B": "Salesforce Tower",
+        "C": "The Burj Khalifa",
+        "D": "The Pentagon"
+    },
+    {
+        "text": "Pick the correct parameter placement",
+        "correct_answer": "function(parameter){}",
+        "A": "function () parameter {}",
+        "B": "function(parameter){}",
+        "C": "function(parameter{})",
+        "D": "fuction{parameter}"
+    },
+    {
+        "text": "If my flex direction is set to column, what axis will align items move my elements? ",
+        "correct_answer": "New York",
+        "A": "column",
+        "B": "horizontal",
+        "C": "up and down",
+        "D": "align items will not affect the element positioning"
+    },
+    {
+        "text": "Which method does not change the original array",
+        "correct_answer": "slice",
+        "A": "push",
+        "B": "slice",
+        "C": "shift",
+        "D": "splice"
+    },
+    {
+        "text": "Which is not a primitive type",
+        "correct_answer": "Template Literal",
+        "A": "string",
+        "B": "Boolean",
+        "C": "Template Literal",
+        "D": "Null"
+    },
+];
+
 /////////////////////////////// inputs question text 
 function displayQuestions() {
     var question = questions[questionIndex];
@@ -80,12 +73,6 @@ function displayQuestions() {
     var option2 = document.getElementById("answer2")
     var option3 = document.getElementById("answer3")
     var option4 = document.getElementById("answer4")
-
-    console.log("display question" , questionIndex);
-    // if (questionIndex >= questions.length){
-    //     console.log('test')
-    //     // setScore();
-    // }
 
     questionEl.textContent = question.text;
     option1.textContent = question.A;
@@ -107,10 +94,10 @@ document.getElementById("answer1").addEventListener("click", function checkAnswe
        setTime(secondsLeft-5)
     }
     
-
     questionIndex++,
     displayQuestions(questionIndex)
 });
+
 document.getElementById("answer2").addEventListener("click", function checkAnswer2() {
     var question = questions[questionIndex];
     if (this.textContent === question.correct_answer){
@@ -152,24 +139,23 @@ document.getElementById("answer4").addEventListener("click", function checkAnswe
     displayQuestions(questionIndex)
 });
 
+displayQuestions();
 // opens form to add name and score
+var user = []; 
 
 function setScore(){
     var name= window.prompt('Enter your name to view scores')
 
-    var user = {
+    var userArray = {
         firstName: name,
         finalScore: score, 
     }
 
     localStorage.setItem("user", JSON.stringify(user));
     console.log('saved in local storage')
-    renderScores();
-}
 
-function renderScores() {
     var lastGrade = JSON.parse(localStorage.getItem('user'));
-    var history = document.getElementById(scoreList)
+    // var history = document.getElementById(scoreList)
 
     if (lastGrade !== null){
         console.log('pulled from local storage')
@@ -178,21 +164,22 @@ function renderScores() {
         pastScores.style.display = 'block'
         
         var li = document.createElement("li");
-        li.textContent= lastGrade.firstName + " : " + lastGrade.score,
+        li.textContent= lastGrade.firstName + " : " + score,
         pastScores.appendChild(li);
-
-        // document.getElementById('saved-name').innerHTML = lastGrade.firstName;
-        // document.getElementById('saved-grade').innerHTML = lastGrade.score;
     }
-
     var buttons = document.getElementById('buttons')
     buttons.style.display = 'none';
     
     document.getElementById('questionText').textContent= lastGrade.firstName + " received " + score + " out of 5 "
+}
 
+function init(){
+    var storedUser = JSON.parse(localStorage.getItem('user'));
+
+    if (storedUser!== null){
+        user = storedUser;
+    }
 }
 
 
 
-setTime();
-displayQuestions();
